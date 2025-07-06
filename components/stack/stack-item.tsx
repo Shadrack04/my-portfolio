abd6627398ec624459cfe6289ebf83b3a4f301ca
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-06 09:00:31 +0100

    align the about section on mobile

--- components/stack/stack-item.tsx
+++ components/stack/stack-item.tsx
@@ -3,7 +3,7 @@ import { FaReact } from "react-icons/fa";
 
 export default function StackItem() {
   return (
-    <div className=" card-shadow border-primary-color border-1  bg-[#ECFAE5] w-[16rem] hover:scale-105 transition-all duration-300 flex items-center flex-col py-6 px-4 rounded-2xl">
+    <div className=" card-shadow border-primary-color border-1  bg-[#ECFAE5] md:w-[16rem] hover:scale-105 transition-all duration-300 flex items-center flex-col py-6 px-4 rounded-2xl">
       <FaReact className=" p-2 rounded-full bg-white text-primary-button size-20 mb-2" />
       <p className=" text-primary-text font-bold text-2xl my-4">React.Js</p>
       <p className=" text-content-text  text-center">
