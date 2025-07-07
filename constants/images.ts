379b951fa024b7e4aa2371ab0d6dce750f8fca64
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-07 03:59:27 +0100

    fixes

--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,5 @@
-import heroImage from "@/assets/images/hero-image.webp";
+// import heroImage from "@/assets/images/hero-image.webp";
+import heroImage from "@/public/assets/images/hero-image.webp";
 
 export const images = {
   heroImage,
