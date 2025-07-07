d85e9a621744f1330fbba4ed54227a98307485a3
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-07 22:34:30 +0100

    add floating element component

---
+++ components/stack/floating-element.tsx
@@ -0,0 +1,9 @@
+import FloatElements from "@/components/ui/floating-elements-demo";
+
+export default function FloatingElementsDemo() {
+  return (
+    <FloatElements title="Trusted by" className="mt-20">
+      {/* Your floating elements here */}
+    </FloatElements>
+  );
+}
