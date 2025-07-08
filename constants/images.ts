96d1465092dd230efdcb0359ba30d7a630420363
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-08 23:42:41 +0100

    move images into public

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,5 @@
 // import heroImage from "@/assets/images/hero-image.webp";
-import heroImage from "@/assets/images/hero-image2.webp";
+// import heroImage from "@/assets/images/hero-image2.webp";
 import kdnOg from "@/assets/images/kdn-og.webp";
 // import kdn_hero_image from "@/assets/images/kdn-hero-image.webp";
 
