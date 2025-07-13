949c7f146c0bd259b71ba81c68f6a2ee61440be3
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-13 08:00:39 +0100

    fix type error

--- components/animation/animation-wrapper.tsx
+++ components/animation/animation-wrapper.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import React, { ElementType } from "react";
+import React, { ElementType, ReactNode } from "react";
 import { motion, type MotionProps } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 interface AnimationWrapperProps extends MotionProps {
-  children: React.ReactNode;
+  children: ReactNode;
   // as?: keyof JSX.IntrinsicElements;
   as?: ElementType;
   className?: string;
+  [key: string]: any;
 }
 
 export default function AnimationWrapper({
@@ -17,7 +18,13 @@ export default function AnimationWrapper({
   className,
   ...MotionProps
 }: AnimationWrapperProps) {
-  const MotionTag = motion[as || "div"];
+  // const MotionTag = motion[as || "div"];
+  const isStringTag = typeof as === "string";
+  // const MotionTag = isStringTag ? motion[as] : motion(as);
+  const MotionTag: ElementType =
+    isStringTag && as in motion
+      ? (motion[as as keyof typeof motion] as ElementType)
+      : motion(as);
 
   return (
     <MotionTag className={cn(className)} {...MotionProps}>
