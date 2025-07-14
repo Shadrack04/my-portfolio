8929993b04dfa35a79bc94bce93e5761648f6b33
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-14 20:26:59 +0100

    Add active state style to nav list

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,32 +1,50 @@
-import { Database, Home, Send, UserRound } from "lucide-react";
-import Link from "next/link";
+"use client";
 
 import React from "react";
+import { Database, Home, Send, UserRound } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className=" flex items-center justify-center z-90">
       <ul className="nav-shadow flex bg-foreground z-99 items-center text-[90%] justify-center fixed bottom-6 gap-4 md:gap-6 px-4 py-3 rounded-full">
-        <li className=" hover:text-primary-button text-primary-text transition-all duration-400 cursor-pointer hover:font-bold">
-          <Link href="/" className="flex items-center gap-1">
+        <li
+          className={` hover:text-primary-color text-primary-text transition-all duration-400 cursor-pointer hover:font-bold`}
+        >
+          <Link
+            href="/"
+            className={` ${pathname == "/" ? " text-primary-button underline underline-offset-8 decoration-primary-color font-bold " : ""}flex items-center gap-1`}
+          >
             <Home className=" size-4" />
             Home
           </Link>
         </li>
         <li className="hover:text-primary-button transition-all text-primary-text duration-400 cursor-pointer hover:font-bold flex items-center gap-1">
-          <a href="/about" className="flex items-center gap-1">
+          <a
+            href="/about"
+            className={` ${pathname == "/about" ? " text-primary-button underline underline-offset-8 decoration-primary-color font-bold " : ""}flex items-center gap-1`}
+          >
             <UserRound className=" size-4" />
             About
           </a>
         </li>
         <li className="hover:text-primary-button transition-all text-primary-text duration-400 cursor-pointer hover:font-bold flex items-center gap-1">
-          <a href="/projects" className="flex items-center gap-1">
+          <a
+            href="/projects"
+            className={` ${pathname == "/projects" ? " text-primary-button underline underline-offset-8 decoration-primary-color font-bold " : ""}flex items-center gap-1`}
+          >
             <Database className=" size-4" />
             Project
           </a>
         </li>
         <li className="hover:text-primary-button transition-all text-primary-text duration-400 cursor-pointer hover:font-bold flex items-center gap-1">
-          <a href="/contact" className="flex items-center gap-1">
+          <a
+            href="/contact"
+            className={` ${pathname == "/contact" ? " text-primary-button underline underline-offset-8 decoration-primary-color font-bold " : ""}flex items-center gap-1`}
+          >
             <Send className=" size-4" />
             Contact
           </a>
