decddec3daeaabe2494ba1992fa02325bd4e1845
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-07-15 18:56:32 +0100

    fixes

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -17,7 +17,7 @@ export default function Projects() {
         </p>
       </div>
 
-      <div className=" my-12 grid gap-y-20  md:px-6  place-items-center">
+      <div className=" my-12 grid gap-y-20  md:px-1  place-items-center">
         {[1, 2, 3].map((_, index) => (
           <ProjectItem key={index} index={index} />
         ))}
