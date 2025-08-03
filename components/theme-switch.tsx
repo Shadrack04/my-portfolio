8e2fee5d08dba9ea715604447448fdff5073a93b
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-08-03 19:25:18 +0100

    fix hydration issue

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -7,12 +7,17 @@ import { Switch } from "@/components/ui/switch";
 
 export default function ThemeSwitch() {
   const id = useId();
-  const [checked, setChecked] = useState(() => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("theme") === "dark";
-    }
-    return false;
-  });
+  const [hasMounted, setHasMounted] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const isDark = savedTheme == "dark";
+
+    setChecked(isDark);
+    document.documentElement.classList.toggle("dark", isDark);
+    setHasMounted(true);
+  }, []);
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", checked);
@@ -36,13 +41,16 @@ export default function ThemeSwitch() {
       >
         <SunIcon size={16} aria-hidden="true" />
       </span>
-      <Switch
-        id={id}
-        checked={checked}
-        onCheckedChange={toggleSwitch}
-        aria-labelledby={`${id}-off ${id}-on`}
-        aria-label="Toggle between dark and light mode"
-      />
+      {hasMounted && (
+        <Switch
+          id={id}
+          checked={checked}
+          onCheckedChange={toggleSwitch}
+          aria-labelledby={`${id}-off ${id}-on`}
+          aria-label="Toggle between dark and light mode"
+        />
+      )}
+
       <span
         id={`${id}-on`}
         className="hover:text-primary-color text-primary-text group-data-[state=unchecked]:text-muted-foreground/70 flex-1 cursor-pointer text-left text-sm font-medium"
